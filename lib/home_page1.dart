c9e1ba08ad142af1b2f67b6ab00d21a9790f18a8
tayeb ahmed <tayebahmed@gmail.com>
2022-04-03 10:37:44 +0600

    create home_page1

---
+++ lib/home_page1.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePage1 extends StatefulWidget {
+  const HomePage1({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage1> createState() => _HomePage1State();
+}
+
+class _HomePage1State extends State<HomePage1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HomePage1'),
+      ),
+    );
+  }
+}
